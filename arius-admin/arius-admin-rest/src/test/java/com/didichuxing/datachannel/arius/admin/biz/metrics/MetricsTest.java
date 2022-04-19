af888bc07238b3c2bbf4ee7e5095b7b1f78f045b
shizeying <w741069229@gmail.com>
2022-04-19 11:06:36 +0800

    修改pom文件，统一maven版本配置

---
+++ arius-admin/arius-admin-rest/src/test/java/com/didichuxing/datachannel/arius/admin/biz/metrics/MetricsTest.java
@@ -0,0 +1,36 @@
+package com.didichuxing.datachannel.arius.admin.biz.metrics;
+
+import com.alibaba.fastjson.JSON;
+import com.didichuxing.datachannel.arius.admin.AriusAdminApplicationTest;
+import com.didichuxing.datachannel.arius.admin.client.bean.common.Result;
+import com.didichuxing.datachannel.arius.admin.client.bean.dto.metrics.MetricsClusterPhyDTO;
+import com.didichuxing.datachannel.arius.admin.common.constant.metrics.ClusterPhyTypeMetricsEnum;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Rollback
+public class MetricsTest extends AriusAdminApplicationTest {
+	@Autowired
+	private ClusterPhyMetricsManager clusterPhyMetricsManager;
+	
+	@Test
+	public void getClusterMetricsByMetricsTypeTest(){
+		MetricsClusterPhyDTO param=new MetricsClusterPhyDTO();
+		param.setClusterPhyName("yyf-test-jr-1228");
+		param.setStartTime(1650003983761L);
+		param.setEndTime(1650007583761L);
+		param.setAggType("max");
+		param.setMetricsTypes(Arrays.asList("basic"));
+		param.setTopNu(5);
+		
+		final Result<Object> admin = clusterPhyMetricsManager.getClusterMetricsByMetricsType(param, 1,
+				"admin",
+				ClusterPhyTypeMetricsEnum.CLUSTER);
+		System.out.println(JSON.toJSONString(admin,true));
+	}
+	
+}
