63df07384a357ff331afa7bc7db52d0531a658f5
chengxiang <liuchengxiang_i@didiglobal.com>
2022-04-25 16:03:20 +0800

    refactor: remove didi pipeline

--- arius-admin/arius-admin-rest/APP_META/clean_nginx_log.sh
+++
@@ -1,6 +0,0 @@
-log_dir="/home/xiaoju/nginx/logs"
-date=`date +%Y%m%d`
-date_rm=`date +%Y%m%d -d '3 days ago'`
-mv ${log_dir}/access.log  ${log_dir}/${date}_access.log
-/home/xiaoju/nginx/sbin/nginx -s reopen
-rm -f ${log_dir}/${date_rm}_access.log
