4c5706ef10dcf5c12d34b74945c50b6f0204fbfa
chengxiang <liuchengxiang_i@didiglobal.com>
2022-05-12 15:13:48 +0800

    feat: add preCreateIndex cron task

--- arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/template/new_srv/precreate/impl/PreCreateManagerImpl.java
+++ arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/template/new_srv/precreate/impl/PreCreateManagerImpl.java
@@ -14,6 +14,7 @@ import com.didichuxing.datachannel.arius.admin.common.util.IndexNameFactory;
 import com.didichuxing.datachannel.arius.admin.core.service.es.ESIndexService;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -21,6 +22,7 @@ import java.util.List;
  * @author chengxiang, zqr
  * @date 2022/5/11
  */
+@Service
 public class PreCreateManagerImpl extends BaseTemplateSrvImpl implements PreCreateManager {
 
     private final Integer RETRY_TIMES = 3;
---
+++ arius-admin/arius-admin-task/src/main/java/com/didichuxing/datachannel/arius/admin/task/template/new_srv/PreCreateIndexTask.java
@@ -0,0 +1,55 @@
+package com.didichuxing.datachannel.arius.admin.task.template.new_srv;
+
+
+import com.didichuxing.datachannel.arius.admin.biz.template.new_srv.precreate.PreCreateManager;
+import com.didichuxing.datachannel.arius.admin.task.BaseConcurrentTemplateTask;
+import com.didiglobal.logi.job.annotation.Task;
+import com.didiglobal.logi.job.common.TaskResult;
+import com.didiglobal.logi.job.core.consensual.ConsensualEnum;
+import com.didiglobal.logi.job.core.job.Job;
+import com.didiglobal.logi.job.core.job.JobContext;
+import com.didiglobal.logi.log.ILog;
+import com.didiglobal.logi.log.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author chengxiang
+ * @date 2022/5/12
+ */
+@Task(name = "preCreateIndexTask", description = "明天索引预先创建任务，预先创建平台中模板明天索引，避免凌晨大规模创建索引", cron = "0 0 03-06 * * ?", autoRegister = true, consensual = ConsensualEnum.RANDOM)
+public class PreCreateIndexTask extends BaseConcurrentTemplateTask implements Job {
+
+    private static final ILog LOGGER = LogFactory.getLog(IndexRolloverTask.class);
+
+    @Autowired
+    private PreCreateManager preCreateManager;
+
+    @Override
+    public TaskResult execute(JobContext jobContext) throws Exception {
+        LOGGER.info("class=PreCreateIndexTask||method=execute||msg=PreCreateIndexTask start");
+        if (execute()) {
+            return TaskResult.SUCCESS;
+        }
+        return TaskResult.FAIL;
+    }
+
+    @Override
+    public String getTaskName() {
+        return "PreCreateIndexTask";
+    }
+
+    @Override
+    public int poolSize() {
+        return 10;
+    }
+
+    @Override
+    public int current() {
+        return 5;
+    }
+
+    @Override
+    protected boolean executeByLogicTemplate(Integer logicId) {
+        return preCreateManager.preCreateIndex(logicId).success();
+    }
+}
