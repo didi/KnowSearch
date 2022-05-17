ce8211a21c653e4f5804efa8112cd906ef15e19d
chengxiang <abstract_liu@zju.edu.cn>
2022-05-17 16:02:17 +0800

    style: add annotation to templateSrvManager

--- arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/template/new_srv/TemplateSrvManager.java
+++ arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/template/new_srv/TemplateSrvManager.java
@@ -2,14 +2,18 @@ package com.didichuxing.datachannel.arius.admin.biz.template.new_srv;
 
 import com.didichuxing.datachannel.arius.admin.common.bean.common.Result;
 import com.didichuxing.datachannel.arius.admin.common.bean.entity.template.TemplateSrv;
-
 import java.util.List;
 
+/**
+ * @author chengxiang
+ * @date 2022/5/17
+ */
 public interface TemplateSrvManager {
 
     /**
      * 判断指定逻辑模板是否开启了该模板服务
-     * @param logicTemplateId 逻辑模板id, templateSrvId 模板服务id
+     * @param logicTemplateId 逻辑模板id
+     * @param templateSrvId 模板服务id
      * @return
      */
     boolean isTemplateSrvOpen(Integer logicTemplateId, Integer templateSrvId);
