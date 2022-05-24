464e8ca33db6524b58cdbb8e12a46b40c0c6b994
chengxiang <abstract_liu@zju.edu.cn>
2022-05-24 15:56:41 +0800

    fix: preCreate index name with version

--- arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/template/new_srv/precreate/impl/PreCreateManagerImpl.java
+++ arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/template/new_srv/precreate/impl/PreCreateManagerImpl.java
@@ -143,8 +143,8 @@ public class PreCreateManagerImpl extends BaseTemplateSrvImpl implements PreCrea
             return true;
         }
 
-        String tomorrowIndexName = IndexNameFactory.getNoVersion(physicalWithLogic.getExpression(),
-                physicalWithLogic.getLogicTemplate().getDateFormat(), 1);
+        String tomorrowIndexName = IndexNameFactory.get(physicalWithLogic.getExpression(),
+                physicalWithLogic.getLogicTemplate().getDateFormat(), 1, physicalWithLogic.getVersion());
         return esIndexService.syncCreateIndex(physicalWithLogic.getCluster(), tomorrowIndexName, retryCount);
     }
 
