30a2a6968cb70ca0b766ae8de9ba5e62dd0e4e3c
chengxiang <abstract_liu@zju.edu.cn>
2022-06-06 10:21:39 +0800

    feat: add target index

--- arius-admin/arius-admin-common/src/main/java/com/didichuxing/datachannel/arius/admin/common/bean/dto/indices/manage/IndexCatCellWithConfigDTO.java
+++ arius-admin/arius-admin-common/src/main/java/com/didichuxing/datachannel/arius/admin/common/bean/dto/indices/manage/IndexCatCellWithConfigDTO.java
@@ -26,6 +26,9 @@ public class IndexCatCellWithConfigDTO extends IndexCatCellDTO {
     @ApiModelProperty("alias")
     private String alias;
 
+    @ApiModelProperty("target index")
+    private String targetIndex;
+
     @ApiModelProperty("extra")
     private String extra;
 }
