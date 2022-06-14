cdbfa316c43e98c16d173990f9584eed793c9055
linyunan <linyunan_i@didichuxing.com>
2022-06-14 16:28:52 +0800

    bug fix : 模板分页接口

--- arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/page/AbstractPageSearchHandle.java
+++ arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/page/AbstractPageSearchHandle.java
@@ -17,7 +17,6 @@ public abstract class AbstractPageSearchHandle<T extends PageDTO, R> implements 
      * @return            PaginationResult<R>
      */
     public PaginationResult<R> doPage(T condition, Integer appId) {
-
         Result<Boolean> validCheckForConditionResult = checkCondition(condition, appId);
         if (validCheckForConditionResult.failed()) {
             return PaginationResult.buildParamIllegal(validCheckForConditionResult.getMessage());
