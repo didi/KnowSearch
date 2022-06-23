1d584add83f8c423a7f38a3fd8c08989919384cb
nicolewu_i <nicolewu_i@didiglobal.com>
2022-06-23 14:07:08 +0800

    完成gateway模块中与admin关联的业务的单测

--- arius-gateway/arius-gateway-rest/src/test/java/com/didi/arius/gateway/util/CustomDataSource.java
+++ arius-gateway/arius-gateway-rest/src/test/java/com/didi/arius/gateway/util/CustomDataSource.java
@@ -3,9 +3,16 @@ package com.didi.arius.gateway.util;
 import com.didi.arius.gateway.common.consts.QueryConsts;
 import com.didi.arius.gateway.common.metadata.*;
 import com.didi.arius.gateway.elasticsearch.client.ESClient;
+import com.didi.arius.gateway.elasticsearch.client.gateway.direct.DirectRequest;
+import com.didi.arius.gateway.elasticsearch.client.gateway.direct.DirectResponse;
 import com.didi.arius.gateway.remote.response.*;
+import org.apache.http.Header;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.rest.RestStatus;
 
 import java.util.*;
+
+import static com.didi.arius.gateway.common.metadata.AppDetail.RequestType.CLUSTER;
 
 /**
  * @author wuxuan
@@ -17,15 +24,57 @@ public class CustomDataSource {
     public static final String ip = "127.0.0.0" ;
     public static final String INDEX_NAME = "cn_record.arius.template.value";
     public static final String INDEX_NAME2 = "cn_record.arius.template.value_2021-05";
+    public static final String INDEX_NAME3 = "cn_record.arius.template.value_2022-06";
+    public static final String ALIAS = "ALIAS";
     public static final int appid = 1;
 
+    public static AppListResponse appListResponseFactory() {
+        AppListResponse appListResponse = new AppListResponse();
+        appListResponse.setCode(1);
+        appListResponse.setMessage("message");
+        appListResponse.setVersion("1.2.2");
+        List<AppDetailResponse> data = new ArrayList<>();
+        data.add(appDetailResponseFactory());
+        appListResponse.setData(data);
+        return appListResponse;
+    }
+
+    public static AppDetailResponse appDetailResponseFactory() {
+        AppDetailResponse appDetailResponse = new AppDetailResponse();
+        appDetailResponse.setCluster(CLUSTER_NAME);
+        appDetailResponse.setQueryThreshold(1);
+        appDetailResponse.setId(appid);
+        appDetailResponse.setVerifyCode("789");
+        List<String> ips = new ArrayList<>();
+        ips.add(ip);
+        appDetailResponse.setIp(ips);
+        return appDetailResponse;
+    }
+
+    public static BaseContext baseContextFactory() {
+        BaseContext baseContext = queryContextFactory();
+        baseContext.setAuthentication("Basic MTo3ODk=");
+        baseContext.setAppid(appid);
+        baseContext.setAppDetail(appDetailFactory());
+        return baseContext;
+    }
     public static AppDetail appDetailFactory() {
         AppDetail appDetail = new AppDetail();
         appDetail.setId(appid);
         appDetail.setCluster(PHY_CLUSTER_NAME);
+        appDetail.setSearchType(CLUSTER);
         List<String> ips = new ArrayList<String>();
         ips.add(ip);
         appDetail.setIp(ips);
+        List<String> windexexp =new ArrayList<>();
+        windexexp.add(INDEX_NAME);
+        windexexp.add(INDEX_NAME2);
+        List<String> indexexp =new ArrayList<>();
+        indexexp.add(INDEX_NAME);
+        indexexp.add(INDEX_NAME2);
+        appDetail.setWindexExp(windexexp);
+        appDetail.setIndexExp(indexexp);
+        appDetail.setIsRoot(0);
         return appDetail;
     }
 
@@ -176,11 +225,20 @@ public class CustomDataSource {
 
     public static BaseInfoResponse baseInfoResponseFactory() {
         BaseInfoResponse baseInfoResponse = new BaseInfoResponse();
-        baseInfoResponse.setExpression("expression");
+        baseInfoResponse.setExpireTime(1L);
+        baseInfoResponse.setExpression("expression*");
+        baseInfoResponse.setIsDefaultRouting(true);
+        baseInfoResponse.setDeployStatus(1);
+        List<String> aliases = new ArrayList<>();
+        aliases.add(INDEX_NAME);
+        aliases.add(INDEX_NAME2);
+        aliases.add(ALIAS);
+        baseInfoResponse.setAliases(aliases);
+        baseInfoResponse.setIngestPipeline("ingestpipeline");
         baseInfoResponse.setId(1);
-        baseInfoResponse.setBlockRead(true);
+        baseInfoResponse.setBlockRead(false);
         baseInfoResponse.setDateField("datafiled");
-        baseInfoResponse.setBlockWrite(true);
+        baseInfoResponse.setBlockWrite(false);
         baseInfoResponse.setDateFormat("dataformat");
         baseInfoResponse.setVersion(1);
         return baseInfoResponse;
@@ -189,21 +247,44 @@ public class CustomDataSource {
     public static MasterInfoResponse masterInfoResponseFactory(){
         MasterInfoResponse masterInfoResponse = new MasterInfoResponse();
         masterInfoResponse.setCluster(CLUSTER_NAME);
+        List<Integer> accessApps = new ArrayList<>();
+        accessApps.add(1);
+        accessApps.add(2);
+        masterInfoResponse.setAccessApps(accessApps);
+        masterInfoResponse.setTopic("topic");
+        masterInfoResponse.setMappingIndexNameEnable(true);
+        Map<String, String> typeIndexMapping = new HashMap<>();
+        typeIndexMapping.put(CLUSTER_NAME,INDEX_NAME);
+        masterInfoResponse.setTypeIndexMapping(typeIndexMapping);
+        masterInfoResponse.setTemplateId(1L);
+        masterInfoResponse.setTemplateName("templateName");
         return masterInfoResponse;
     }
 
-    public static List<SlaveInfoResponse> slaveInfosFactory() {
-        List<SlaveInfoResponse> slaveInfos = new ArrayList<>();
+    public static List<SlaveInfoResponse> slaveInfoResponsesFactory() {
+        List<SlaveInfoResponse> slaveInfoResponses = new ArrayList<>();
         SlaveInfoResponse slaveInfoResponse = new SlaveInfoResponse();
+        List<Integer> accessApps = new ArrayList<>();
+        accessApps.add(1);
+        accessApps.add(2);
+        slaveInfoResponse.setAccessApps(accessApps);
         slaveInfoResponse.setCluster(CLUSTER_NAME);
-        return slaveInfos;
+        slaveInfoResponse.setTopic("topic");
+        slaveInfoResponse.setMappingIndexNameEnable(true);
+        slaveInfoResponse.setTemplateId(1L);
+        slaveInfoResponse.setTemplateName("templateName");
+        Map<String, String> typeIndexMapping = new HashMap<>();
+        typeIndexMapping.put(CLUSTER_NAME,INDEX_NAME);
+        slaveInfoResponse.setTypeIndexMapping(typeIndexMapping);
+        slaveInfoResponses.add(slaveInfoResponse);
+        return slaveInfoResponses;
     }
 
     public static IndexTemplateResponse indexTemplateResponseFactory(){
         IndexTemplateResponse indexTemplateResponse = new IndexTemplateResponse();
         indexTemplateResponse.setBaseInfo(baseInfoResponseFactory());
         indexTemplateResponse.setMasterInfo(masterInfoResponseFactory());
-        indexTemplateResponse.setSlaveInfos(slaveInfosFactory());
+        indexTemplateResponse.setSlaveInfos(slaveInfoResponsesFactory());
         return indexTemplateResponse;
     }
 
@@ -214,6 +295,8 @@ public class CustomDataSource {
         IndexTemplateResponse indexTemplateResponse = indexTemplateResponseFactory();
         data.put("key",indexTemplateResponseFactory());
         indexTemplateListResponse.setData(data);
+        indexTemplateListResponse.setCode(1);
+        indexTemplateListResponse.setVersion("1.3.2");
         return indexTemplateListResponse;
     }
 
@@ -235,6 +318,22 @@ public class CustomDataSource {
         return templateInfoListResponse;
     }
 
+    public static TemplateInfo templateInfoFactory(){
+        TemplateInfo templateInfo = new TemplateInfo();
+        templateInfo.setNeedSource(true);
+        templateInfo.setVersion(1);
+        return templateInfo;
+    }
+
+    public static TempaletAliasResponse tempaletAliasResponseFactory() {
+        TempaletAliasResponse tempaletAliasResponse = new TempaletAliasResponse();
+        tempaletAliasResponse.setVersion("version");
+        tempaletAliasResponse.setMessage("messgage");
+        tempaletAliasResponse.setCode(1);
+        tempaletAliasResponse.setData(true);
+        return tempaletAliasResponse;
+    }
+
     public static List<AliasesInfoResponse> aliasesFactory() {
         List<AliasesInfoResponse> aliases = new ArrayList<>();
         AliasesInfoResponse aliasesInfoResponse = new AliasesInfoResponse();
@@ -243,10 +342,24 @@ public class CustomDataSource {
         return aliases;
     }
 
+    public static ESClient esClientFactory() {
+        ESClient esClient = new ESClient();
+        esClient.setClusterName(CLUSTER_NAME);
+        esClient.addHttpHost("127.0.0.0",8080);
+        List<Header> headers = new ArrayList<>();
+        Header header = new BasicHeader("name","value");
+        headers.add(header);
+        esClient.setHeaders(headers);
+        esClient.start();
+        return esClient;
+    }
+
     public static ESCluster esClusterFactory() {
         ESCluster esCluster = new ESCluster();
         esCluster.setCluster(CustomDataSource.CLUSTER_NAME);
         esCluster.setType(ESCluster.Type.INDEX);
+        esCluster.setEsVersion(QueryConsts.DEFAULT_ES_VERSION);
+        esCluster.setEsClient(esClientFactory());
         return esCluster;
     }
 
@@ -264,4 +377,21 @@ public class CustomDataSource {
         dslTemplateListResponse.setData(data);
         return dslTemplateListResponse;
     }
+
+    public static Map<String, IndexTemplate> indexTemplateMapFactory() {
+        Map<String, IndexTemplate>  indexTemplateMap = new HashMap<>();
+        indexTemplateMap.put(CustomDataSource.INDEX_NAME,indexTemplateFactory());
+        indexTemplateMap.put(CustomDataSource.INDEX_NAME2,indexTemplateFactory());
+        return indexTemplateMap;
+    }
+
+    public static Map<String, ESCluster> listESClusterFactory() {
+        Map<String, ESCluster> listESCluster = new HashMap<>();
+        listESCluster.put("key",CustomDataSource.esClusterFactory());
+        ESCluster esCluster = CustomDataSource.esClusterFactory();
+        esCluster.setType(ESCluster.Type.SOURCE);
+        listESCluster.put(CLUSTER_NAME,esCluster);
+        return listESCluster;
+    }
+
 }
