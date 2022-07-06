e480bb37869e36554b76d9481ce7d029c8f9c4c4
slhu <ohushenglin_v@didichuxing.com>
2022-07-06 16:07:01 +0800

    refactor：接口优化：/v3/op/worktask/ 相关接口下线，对应使用 /v3/op-task/

--- arius-admin/arius-admin-rest/src/main/java/com/didichuxing/datachannel/arius/admin/rest/controller/v3/op/task/OpTaskController.java
+++ arius-admin/arius-admin-rest/src/main/java/com/didichuxing/datachannel/arius/admin/rest/controller/v3/op/task/OpTaskController.java
@@ -1,7 +1,15 @@
 package com.didichuxing.datachannel.arius.admin.rest.controller.v3.op.task;
 
 import static com.didichuxing.datachannel.arius.admin.common.constant.ApiVersion.V3;
-import static com.didichuxing.datachannel.arius.admin.common.constant.ApiVersion.V3_OP;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import com.alibaba.fastjson.JSON;
 import com.didichuxing.datachannel.arius.admin.biz.task.OpTaskManager;
@@ -16,25 +24,12 @@ import com.didichuxing.datachannel.arius.admin.common.util.ConvertUtil;
 import com.didichuxing.datachannel.arius.admin.common.util.EnvUtil;
 import com.didiglobal.logi.log.ILog;
 import com.didiglobal.logi.log.LogFactory;
-
 import com.didiglobal.logi.security.util.HttpRequestUtil;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author fengqiongfeng
@@ -42,7 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(tags = "任务相关接口(REST)")
 @RestController
-@RequestMapping({ V3_OP + "/worktask", V3 + "/op-task"})
+@RequestMapping({ V3 + "/op-task" })
 public class OpTaskController {
     private static final ILog LOGGER = LogFactory.getLog(OpTaskController.class);
 
