edcb7e1fa37cff1ab8d6419446231453a6148ddf
linyunan <linyunan_i@didichuxing.com>
2022-07-07 18:46:04 +0800

    refactor: 索引管理添加集群黑名单，禁止对集群索引进行操作

---
+++ arius-admin/arius-admin-rest/src/main/java/com/didichuxing/datachannel/arius/admin/rest/controller/v3/op/indices/BaseIndicesController.java
@@ -0,0 +1,45 @@
+package com.didichuxing.datachannel.arius.admin.rest.controller.v3.op.indices;
+
+import com.didichuxing.datachannel.arius.admin.common.bean.common.Result;
+import com.didichuxing.datachannel.arius.admin.core.service.common.AriusConfigInfoService;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by linyunan on 2022/7/7
+ */
+
+public abstract class BaseIndicesController {
+
+    @Autowired
+    private AriusConfigInfoService ariusConfigInfoService;
+
+    /**
+     * 临时黑名单列表, 禁止通过索引管理操作物理集群
+     * @param clusterPhyNames   物理集群列表
+     * @return                  Result<Boolean>
+     */
+    protected Result<Boolean> checkClusterValid(List<String> clusterPhyNames) {
+        if (CollectionUtils.isEmpty(clusterPhyNames)) { return Result.buildSucc();}
+
+        List<String> filterClustersFromBiz = Lists.newArrayList("didi-cluster-test");
+
+        Set<String> filterClustersFromAriusConfig = ariusConfigInfoService.stringSettingSplit2Set(
+                "arius.cluster.blacklist", "cluster.phy.name", "", ",");
+
+        filterClustersFromAriusConfig.addAll(filterClustersFromBiz);
+
+        for (String clusterPhyName : clusterPhyNames) {
+            if (filterClustersFromAriusConfig.contains(clusterPhyName)) {
+                return Result.buildFail(String.format("该物理集群[%s]已添加黑名单, 禁止对集群进行任何新增、编辑、删除等操作",
+                        clusterPhyName));
+            }
+        }
+
+        return Result.buildSucc();
+    }
+}
