33366cb6f9e150cdde04513e2687fbcd9ef5f246
ggzed <1500510117@qq.com>
2022-08-09 16:54:10 +0800

    bugfix:修复快捷命令中indices分布字段显示为空

--- arius-admin/arius-admin-rest/src/main/java/com/didichuxing/datachannel/arius/admin/rest/controller/HealthController.java
+++ arius-admin/arius-admin-rest/src/main/java/com/didichuxing/datachannel/arius/admin/rest/controller/HealthController.java
@@ -1,10 +1,8 @@
 package com.didichuxing.datachannel.arius.admin.rest.controller;
 
 import com.didichuxing.datachannel.arius.admin.common.bean.common.Result;
-import com.didichuxing.datachannel.arius.admin.metadata.job.shard.ShardCatInfoCollector;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,16 +25,4 @@ public class HealthController {
         return Result.buildSuccWithMsg("let's go");
     }
 
-
-    @Autowired
-    private ShardCatInfoCollector shardCatInfoCollector;
-
-    @GetMapping("/test")
-    @ResponseBody
-    @ApiOperation(value = "探活接口", notes = "")
-    public Result<String> dem1o() {
-
-                shardCatInfoCollector.handleJobTask("");
-        return   Result.buildSuccWithMsg("let's go");
-    }
 }
