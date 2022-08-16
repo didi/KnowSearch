a4335683887b68e82e6bdc46448ddc5531f8b4e7
shizeying <w741069229@gmail.com>
2022-08-16 15:57:18 +0800

    feature(模版健康率): 新增模版服务的健康率的展示；新增逻辑模版的健康率的采集任务；数据库中新增healthRate字段

--- arius-admin/arius-admin-task/src/main/java/com/didichuxing/datachannel/arius/admin/task/template/srv/HealthRandomTask.java
+++ arius-admin/arius-admin-task/src/main/java/com/didichuxing/datachannel/arius/admin/task/template/srv/HealthRandomTask.java
@@ -15,7 +15,7 @@ import com.didiglobal.logi.log.LogFactory;
  * @author shizeying
  * @date 2022/08/16
  */
-@Task(name = "HealthRandomTask", description = "模板健康同步任务", cron = "0 0/5 * * * ? *", autoRegister = true)
+@Task(name = "HealthRandomTask", description = "模板健康率同步任务", cron = "0 0/5 * * * ? *", autoRegister = true)
 public class HealthRandomTask extends BaseConcurrentTemplateTask implements Job {
 	private static final ILog LOGGER = LogFactory.getLog(ColdDataMoveRandomTask.class);
 	
@@ -32,5 +32,25 @@ public class HealthRandomTask extends BaseConcurrentTemplateTask implements Job 
 	@Override
 	protected boolean executeByLogicTemplate(Integer logicId) throws AdminOperateException {
 		return templateLogicManager.updateTemplateHealthByLogicId(logicId);
+	}
+	
+	/**
+	 * 任务的线程个数
+	 *
+	 * @return
+	 */
+	@Override
+	public int poolSize() {
+		return 10;
+	}
+	
+	/**
+	 * 并发度
+	 *
+	 * @return
+	 */
+	@Override
+	public int current() {
+		return 5;
 	}
 }
