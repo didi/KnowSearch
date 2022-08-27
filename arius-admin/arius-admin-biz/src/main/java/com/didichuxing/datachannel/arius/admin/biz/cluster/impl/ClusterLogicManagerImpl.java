e645e90edc78ed7a8ef159902b74fd5aae3eeec8
shizeying <w741069229@gmail.com>
2022-08-27 11:29:44 +0800

    fix(主从位点差): 如果没有dcdr链路，则位点差为-1 fix(根据项目id获取逻辑集群与物理集群映射): 接口回退，取消返回green集群的接口，改为前端过滤

--- arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/cluster/impl/ClusterLogicManagerImpl.java
+++ arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/cluster/impl/ClusterLogicManagerImpl.java
@@ -615,21 +615,18 @@ public class ClusterLogicManagerImpl implements ClusterLogicManager {
     }
 
     @Override
-    public Result<List<Tuple<String, ClusterPhyVO>>> getHealthClusterRelationByProjectId(Integer projectId) {
+    public Result<List<Tuple<String, ClusterPhyVO>>> getClusterRelationByProjectId(Integer projectId) {
 
         List<Tuple<String, ClusterPhyVO>> collect;
         if (AuthConstant.SUPER_PROJECT_ID.equals(projectId)) {
             List<ClusterPhy> phyList = clusterPhyService.listAllClusters();
-            collect = phyList.stream()
-                    .filter(clusterPhy ->!Objects.equals(clusterPhy.getHealth(),UNKNOWN.getCode()) )
-                    .map(
+            collect = phyList.stream().map(
                 clusterPhy -> new Tuple<>(clusterPhy.getCluster(), ConvertUtil.obj2Obj(clusterPhy, ClusterPhyVO.class)))
                 .collect(Collectors.toList());
         } else {
             List<ClusterLogic> logicList = clusterLogicService.getOwnedClusterLogicListByProjectId(projectId);
             collect = logicList.stream()
                 .map(clusterLogic -> new Tuple<>(clusterLogic.getName(), getPhyNameByLogic(clusterLogic.getId())))
-                 .filter(tuple2 ->!Objects.equals(tuple2.getV2().getHealth(),UNKNOWN.getCode()) )
                 .collect(Collectors.toList());
         }
         return Result.buildSucc(collect);
