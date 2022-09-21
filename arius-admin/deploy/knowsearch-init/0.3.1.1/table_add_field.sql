bb5783c5ea447925e7aa9d1dfd6d828fc9fb3d7c
cailiangfeng_v <cailiangfeng_v@didiglobal.com>
2022-09-21 16:45:57 +0800

    TO61120A2A-720 comment 初始化查询模板的个性化配置

--- arius-admin/deploy/knowsearch-init/0.3.1.1/table_add_field.sql
+++ arius-admin/deploy/knowsearch-init/0.3.1.1/table_add_field.sql
@@ -6,3 +6,10 @@ alter table `user_config_info` add column config_type int(10) NOT NULL DEFAULT '
 truncate table user_config_info;
 # user_metrics_config_info-》user_config_info
 ALTER TABLE es_manager_test3.user_metrics_config_info RENAME TO es_manager_test3.user_config_info;
+
+INSERT INTO `es_manager_test`.`user_config_info`(`user_name`, `project_id`, `config_type`, `config_info`)
+select DISTINCT t1.user_name,t2.project_id,2,
+                concat('[{\"firstUserConfigType\":\"searchQuery\",\"projectId\":',t2.project_id,',\"secondUserConfigType\":\"searchTemplate\",\"userConfigTypes\":[\"totalCostAvg\",\"totalShardsAvg\"],\"userName\":\"',t1.user_name,'\"}]') as config_info
+from logi_security_user t1 inner join
+     logi_security_user_project t2 on t1.id=t2.user_id and t2.is_delete='0'
+where  t1.is_delete='0';
