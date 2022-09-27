23cdcda7f596473f8af93249d239ac37ec1729fe
shizeying <w741069229@gmail.com>
2022-09-27 11:41:47 +0800

    集群挂掉，esclient未挂掉，导致接口报错的问题进行修复

--- arius-admin/arius-admin-common/src/main/java/com/didichuxing/datachannel/arius/admin/common/util/ParsingExceptionUtils.java
+++ arius-admin/arius-admin-common/src/main/java/com/didichuxing/datachannel/arius/admin/common/util/ParsingExceptionUtils.java
@@ -3,10 +3,12 @@ package com.didichuxing.datachannel.arius.admin.common.util;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.didichuxing.datachannel.arius.admin.common.exception.ESOperateException;
+import com.didichuxing.datachannel.arius.admin.common.exception.NullESClientException;
 import com.didiglobal.logi.elasticsearch.client.model.exception.ESAlreadyExistsException;
 import com.didiglobal.logi.elasticsearch.client.model.exception.ESIndexNotFoundException;
 import com.didiglobal.logi.elasticsearch.client.model.exception.ESIndexTemplateMissingException;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
@@ -23,6 +25,8 @@ import org.elasticsearch.common.util.concurrent.UncategorizedExecutionException;
  * @date 2022/08/17
  */
 public final class ParsingExceptionUtils {
+    private final static String CONNECTION_REFUSED="Connection refused";
+    private final static String CLUSTER_ERROR = "当前操作集群异常";
     private ParsingExceptionUtils(){}
     
  /**
@@ -56,8 +60,11 @@ public final class ParsingExceptionUtils {
      */
     public static void abnormalTermination(Exception e) throws ESOperateException {
         String exception = ParsingExceptionUtils.getESErrorMessageByException(e);
-        if (StringUtils.isNotBlank(exception)) {
+        if (StringUtils.isNotBlank(exception)&&!StringUtils.equals(exception,CLUSTER_ERROR)) {
             throw new ESOperateException(exception);
+        }
+        if (StringUtils.equals(exception,CLUSTER_ERROR)){
+            throw new NullESClientException(exception);
         }
     }
     
@@ -157,6 +164,9 @@ public final class ParsingExceptionUtils {
         if (Objects.nonNull(cause) && cause instanceof ResponseException) {
             
             return getErrorMessageByResponseException((ResponseException) cause).orElse(null);
+        }
+        if (Objects.nonNull(cause) && cause instanceof ConnectException) {
+            return StringUtils.equals(cause.getMessage(), CONNECTION_REFUSED) ? CLUSTER_ERROR : cause.getMessage();
         }
         return null;
     }
