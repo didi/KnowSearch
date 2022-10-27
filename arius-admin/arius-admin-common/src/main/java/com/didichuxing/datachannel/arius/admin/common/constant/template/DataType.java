d26b86418b8f71d9b395aa79f7384320448f3be4
nicolewu_i <nicolewu_i@didiglobal.com>
2022-10-27 14:55:31 +0800

    。

---
+++ arius-admin/arius-admin-common/src/main/java/com/didichuxing/datachannel/arius/admin/common/constant/template/DataType.java
@@ -0,0 +1,16 @@
+package com.didichuxing.datachannel.arius.admin.common.constant.template;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataType {
+    public Integer code ;
+
+    public String desc;
+
+    public String label;
+}
---
+++ arius-admin/arius-admin-metadata/src/main/java/com/didichuxing/datachannel/arius/admin/metadata/service/DataTypeService.java
@@ -0,0 +1,7 @@
+package com.didichuxing.datachannel.arius.admin.metadata.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataTypeService {
+}
