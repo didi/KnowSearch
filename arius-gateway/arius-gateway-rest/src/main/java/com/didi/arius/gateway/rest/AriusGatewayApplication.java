3f3bd70b0b5f354f2ed5256b76fe947a998bb744
huqidong <williamhuqidong@didiglobal.com>
2022-11-19 16:25:21 +0800

    aruis-gateway 支持可观测性改造.

--- arius-gateway/arius-gateway-rest/src/main/java/com/didi/arius/gateway/rest/AriusGatewayApplication.java
+++ arius-gateway/arius-gateway-rest/src/main/java/com/didi/arius/gateway/rest/AriusGatewayApplication.java
@@ -3,8 +3,9 @@ package com.didi.arius.gateway.rest;
 import com.didi.arius.gateway.common.utils.Convert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = {"com.didi.arius.gateway"})
+@SpringBootApplication(scanBasePackages = {"com.didi.arius.gateway", "com.didiglobal.knowframework"}, exclude={DataSourceAutoConfiguration.class})
 public class AriusGatewayApplication {
 
     public static void main(String[] args) {
