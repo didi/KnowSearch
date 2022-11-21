96914945649d7a57a4bf6b48e887de88bfefaf51
shizeying <w741069229@gmail.com>
2022-11-21 17:53:06 +0800

    feature(ECM、sql): 1.es：安装、配置、扩缩容、插件、回滚、升级 完成开发（待测试后修改bug）; 2.gateway：安装、配置、扩缩容、升级、回滚（待测试） 3.新增：定时同步 ECM 任务创建状态定任务：此任务会更新任务状态的同时进行后置的一些任务处理：如：创建：成功后会回写到物理集群列表中 4. sql新增

--- arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/task/handler/op/manager/es/ESClusterRestartTaskHandler.java
+++ arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/task/handler/op/manager/es/ESClusterRestartTaskHandler.java
@@ -1,11 +1,15 @@
 package com.didichuxing.datachannel.arius.admin.biz.task.handler.op.manager.es;
 
 import com.alibaba.fastjson.JSON;
-import com.didichuxing.datachannel.arius.admin.biz.task.op.manager.gateway.GatewayRestartContent;
+import com.didichuxing.datachannel.arius.admin.biz.task.op.manager.es.ClusterRestartContent;
 import com.didichuxing.datachannel.arius.admin.common.bean.common.OperateRecord;
 import com.didichuxing.datachannel.arius.admin.common.bean.common.Result;
 import com.didichuxing.datachannel.arius.admin.common.bean.entity.task.OpTask;
 import com.didichuxing.datachannel.arius.admin.common.constant.task.OpTaskTypeEnum;
+import com.didichuxing.datachannel.arius.admin.common.util.ConvertUtil;
+import com.didiglobal.logi.op.manager.domain.component.entity.value.ComponentGroupConfig;
+import com.didiglobal.logi.op.manager.interfaces.dto.general.GeneralGroupConfigDTO;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 
@@ -21,7 +25,7 @@ public class ESClusterRestartTaskHandler extends AbstractESTaskHandler {
 		
 		@Override
 		protected Result<Void> validatedAddTaskParam(OpTask param) {
-				final GatewayRestartContent content = convertString2Content(param.getExpandData());
+				final ClusterRestartContent content = convertString2Content(param.getExpandData());
 				// 校验 componentId 是否存在
 				final com.didiglobal.logi.op.manager.infrastructure.common.Result<String> result = componentService.queryComponentNameById(
 						content.getComponentId());
@@ -34,7 +38,19 @@ public class ESClusterRestartTaskHandler extends AbstractESTaskHandler {
 		
 		@Override
 		protected Result<Void> initParam(OpTask opTask) {
-				
+				final String expandData = opTask.getExpandData();
+				final ClusterRestartContent content = convertString2Content(expandData);
+				//获取组建配置信息
+				final Integer componentId = content.getComponentId();
+				final com.didiglobal.logi.op.manager.infrastructure.common.Result<List<ComponentGroupConfig>> componentConfigRes = componentService.getComponentConfig(
+						componentId);
+				if (componentConfigRes.failed()){
+						return Result.buildFrom(componentConfigRes);
+				}
+				final List<ComponentGroupConfig> data = componentConfigRes.getData();
+				//设置配置信息
+				content.setGroupConfigList(ConvertUtil.list2List(data, GeneralGroupConfigDTO.class));
+				opTask.setExpandData(JSON.toJSONString(content));
 				return Result.buildSucc();
 		}
 		
@@ -54,8 +70,8 @@ public class ESClusterRestartTaskHandler extends AbstractESTaskHandler {
 		}
 		
 		@Override
-		protected GatewayRestartContent convertString2Content(String expandData) {
-				return JSON.parseObject(expandData, GatewayRestartContent.class);
+		protected ClusterRestartContent convertString2Content(String expandData) {
+				return JSON.parseObject(expandData, ClusterRestartContent.class);
 		}
 		
 		@Override
@@ -64,7 +80,7 @@ public class ESClusterRestartTaskHandler extends AbstractESTaskHandler {
 		}
 		
 		protected String getName(String expandData) {
-				final GatewayRestartContent restartContent = convertString2Content(expandData);
+				final ClusterRestartContent restartContent = convertString2Content(expandData);
 				return componentService.queryComponentNameById(restartContent.getComponentId()).getData();
 		}
 		
