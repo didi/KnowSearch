7e714c9b617ad1b717aede48e89c6a585f2f5841
zyl <zyl051088@163.com>
2022-12-06 11:05:56 +0800

    sql脚本变更

--- arius-admin/deploy/knowsearch-init/0.3.1.2/0.3.1.2.增量.sql
+++ arius-admin/deploy/knowsearch-init/0.3.1.2/0.3.1.2.增量.sql
@@ -3,3 +3,7 @@ alter table es_cluster_phy_info
     add  kibana_address varchar(200) default '' null comment 'kibana外链地址';
 alter table es_cluster_phy_info
     add  cerebro_address varchar(200) default '' null comment 'cerebro外链地址';
+
+# 可观测性组件:
+# 1. 所有logi_security_*表名都改成了kf_security_*
+# 2. logi_task表新增字段"node_name_white_list_str", logi_worker表新增字段"node_name"
