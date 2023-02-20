0075ba5cca98ffe449a0e1f68b6d5d04e1665e78
awangmengwei_v <awangmengwei_v@didiglobal.com>
2023-02-20 17:22:28 +0800

    fix: 判断条件修改

--- arius-console/src/container/modal/project/add-project.tsx
+++ arius-console/src/container/modal/project/add-project.tsx
@@ -252,7 +252,7 @@ const AddOrEditProjectModal = (props: { dispatch: any; cb: any; params: any }) =
             required: true,
             message: "请选择责任人",
             validator: (rule: any, value: any) => {
-             if (!setOwnerIdList.length) return Promise.reject('请选择责任人')
+             if (!ownerIdList.length) return Promise.reject('请选择责任人')
              return Promise.resolve()
             },
           },
