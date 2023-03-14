388aa4fa74363306522ffbaaf61462d7f7fdca39
Luckywustone <115451970+Luckywustone@users.noreply.github.com>
2023-03-14 10:33:25 +0800

    对 ClusterContextManager进行相关完善
    对 ClusterContextManager相关注释进行完善，使代码更容易理解

--- arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/cluster/ClusterContextManager.java
+++ arius-admin/arius-admin-biz/src/main/java/com/didichuxing/datachannel/arius/admin/biz/cluster/ClusterContextManager.java
@@ -17,12 +17,14 @@ public interface ClusterContextManager {
     /**
      * 刷新物理集群上下文
      * @param clusterPhyName  物理集群名称
+     * @return ClusterPhyContext 物理集群上下文
      */
     ClusterPhyContext flushClusterPhyContext(String clusterPhyName);
 
     /**
      * 刷新逻辑集群上下文
      * @param clusterLogicId   逻辑集群Id
+     * @return ClusterLogicContext 逻辑集群上下文     
      */
     ClusterLogicContext flushClusterLogicContext(Long clusterLogicId);
 
@@ -34,30 +36,43 @@ public interface ClusterContextManager {
 
     /**
      * 校验逻辑集群是可否关联物理集群, 不同类型的逻辑集群, 校验规则不一样
+     * @param clusterLogicId
+     * @param clusterPhyName
+     * @param regionId
+     * @param clusterLogicType     
      */
     Result<Boolean> canClusterLogicAssociatedPhyCluster(Long clusterLogicId, String clusterPhyName, Long regionId,
                                                         Integer clusterLogicType);
 
     /**
      * 获取可关联的物理集群名称列表, 针对新建逻辑集群、逻辑集群关联region等操作
+     * @param clusterLogicIdclusterLogicType 
+     * @param clusterLogicId
      */
     Result<List<String>> getCanBeAssociatedClustersPhys(Integer clusterLogicType, Long clusterLogicId);
 
     /**
      * 获取集群关联逻辑集群名称列表
+     * @param clusterPhyName     
      */
     List<String> getClusterPhyAssociatedClusterLogicNames(String clusterPhyName);
 
     /**
      * 构建物理集群上下文
      * @param cluster
-     * @return
+     * @return ClusterPhyContext 
      */
     ClusterPhyContext getClusterPhyContext(String cluster);
 
+     /**
+     * 从缓存中获取物理集群上下文
+     * @param cluster
+     * @return ClusterPhyContext 
+     */
     ClusterPhyContext getClusterPhyContextCache(String cluster);
 
     /**
+     * 获取物理集群对应的上下文信息Map
      * @return key-> 物理集群名称, value 上下文信息
      */
     Map<String, ClusterPhyContext> listClusterPhyContextMap();
@@ -65,15 +80,15 @@ public interface ClusterContextManager {
     /**
      * 从缓存中获取逻辑集群上下文
      *
-     * @param clusterLogicId
-     * @return
+     * @param clusterLogicId 逻辑集群Id
+     * @return ClusterLogicContext 逻辑集群上下文
      */
     ClusterLogicContext getClusterLogicContextCache(Long clusterLogicId);
 
     /**
      * 构建逻辑集群上下文
-     * @param clusterLogicId
-     * @return
+     * @param clusterLogicId 逻辑集群Id
+     * @return ClusterLogicContext 逻辑集群上下文
      */
     ClusterLogicContext getClusterLogicContext(Long clusterLogicId);
 }
